ac06e6041fd813975bed166bbf40051d24c1f1de
Admin <Admin@MSI>
2025-05-21 21:08:04 +0800

    Langging ni Hanni

--- src/dataanalyzer/DataAnalyzer.java
+++ src/dataanalyzer/DataAnalyzer.java
@@ -1,9 +1,13 @@
 package dataanalyzer;
 
+import welcomeFrame.startScreen;
+
 public class DataAnalyzer {
 
     public static void main(String[] args) {
+        startScreen ss = new startScreen();
         
+        ss.setVisible(true);
     }
     
 }
